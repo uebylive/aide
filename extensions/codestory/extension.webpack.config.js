ae0bf6f4fcd15dbd73fa5fa32ca8b9a417ab52d4
Naresh <ghostwriternr@gmail.com>
2023-08-07 13:27:29 -0700

    Add webpack config again

--- extensions/codestory/extension.webpack.config.js
+++ extensions/codestory/extension.webpack.config.js
@@ -2,16 +2,19 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-// @ts-check
-const path = require('path');
 
-const srcDir = path.join(__dirname, 'webview-ui-sideview');
-const outDir = path.join(__dirname, 'webview-ui-sideview/build/assets');
+//@ts-check
 
-require('../esbuild-webview-common').run({
-	entryPoints: [
-		path.join(srcDir, 'src/index.tsx'),
-	],
-	srcDir,
-	outdir: outDir,
-}, process.argv);
+'use strict';
+
+const withDefaults = require('../shared.webpack.config');
+
+module.exports = withDefaults({
+	context: __dirname,
+	resolve: {
+		mainFields: ['module', 'main']
+	},
+	entry: {
+		extension: './src/extension.ts',
+	}
+});
