60de3a5b1ddffdcbe26fb756fe8cb3d11f40ea0a
skcd <sandeepkumarpani888@gmail.com>
2023-08-12 00:02:41 -0700

    [jest] list out all the tests in the framework

--- extensions/codestory/src/test_framework/jest.ts
+++
@@ -1,6 +0,0 @@
-// We are going to support the jest testing framework here
-// jest config is present in either package.json or jest.config.js
-// in package.json it is under the jest key
-// in jest.config.js it is under the module.exports key
-// I want to get the path to the jest config file
-// and also the location for the tests
