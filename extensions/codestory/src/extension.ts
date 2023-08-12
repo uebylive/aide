62a9d155b82e63990767d77d4292107fb3d9c950
skcd <sandeepkumarpani888@gmail.com>
2023-08-12 16:25:30 -0700

    [extension] download and setup the anton backend

--- extensions/codestory/src/extension.ts
+++ extensions/codestory/src/extension.ts
@@ -7,7 +7,7 @@ import logger from "./logger";
 import { generateCodeGraph } from "./codeGraph/graph";
 import { EmbeddingsSearch } from "./codeGraph/embeddingsSearch";
 import postHogClient from "./posthog/client";
-import { AgentViewProvider } from './views/AgentView';
+import { AgentViewProvider } from "./views/AgentView";
 import { CodeStoryViewProvider } from "./views/codeStoryView";
 import { healthCheck } from "./subscriptions/health";
 import { openFile, search } from "./subscriptions/search";
@@ -19,203 +19,183 @@ import { triggerCodeSymbolChange } from "./activeChanges/timeline";
 import { gitCommit } from "./subscriptions/gitCommit";
 import { getGitCurrentHash, getGitRepoName } from "./git/helper";
 import { debug } from "./subscriptions/debug";
-import { copySettings } from './utilities/copySettings';
-
+import { copySettings } from "./utilities/copySettings";
 
 import { EventEmitter } from "events";
-import { readActiveDirectoriesConfiguration } from './utilities/activeDirectories';
+import { readActiveDirectoriesConfiguration } from "./utilities/activeDirectories";
 
 class ProgressiveIndexer {
-	private emitter: EventEmitter;
+  private emitter: EventEmitter;
 
-	constructor() {
-		this.emitter = new EventEmitter();
-	}
+  constructor() {
+    this.emitter = new EventEmitter();
+  }
 
-	async indexRepository(
-		storage: CodeStoryStorage,
-		projectManagement: TSMorphProjectManagement,
-		globalStorageUri: string,
-		workingDirectory: string,
-	) {
-		await indexRepository(
-			storage,
-			projectManagement,
-			globalStorageUri,
-			workingDirectory,
-			this.emitter,
-		);
-	}
+  async indexRepository(
+    storage: CodeStoryStorage,
+    projectManagement: TSMorphProjectManagement,
+    globalStorageUri: string,
+    workingDirectory: string
+  ) {
+    await indexRepository(
+      storage,
+      projectManagement,
+      globalStorageUri,
+      workingDirectory,
+      this.emitter
+    );
+  }
 
-	on(event: string, listener: (...args: any[]) => void) {
-		this.emitter.on(event, listener);
-	}
+  on(event: string, listener: (...args: any[]) => void) {
+    this.emitter.on(event, listener);
+  }
 }
 
 export async function activate(context: ExtensionContext) {
-	// Project root here
-	logger.info("[CodeStory] Chat view command registered");
-	logger.info(context.extensionUri);
-	logger.info(context.globalStorageUri.fsPath);
-	logger.info("[CodeStory] skcd debugging");
-	postHogClient.capture({
-		distinctId: env.machineId,
-		event: "extension_activated",
-	});
-	let rootPath = workspace.rootPath;
-	if (!rootPath) {
-		rootPath = "";
-	}
-	if (rootPath === "") {
-		window.showErrorMessage("Please open a folder in VS Code to use CodeStory");
-		return;
-	}
-	const repoName = await getGitRepoName(
-		rootPath,
-	);
-	const repoHash = await getGitCurrentHash(
-		rootPath,
-	);
-	// Get the storage object here
-	const codeStoryStorage = await loadOrSaveToStorage(context.globalStorageUri.fsPath, rootPath);
-	logger.info(codeStoryStorage);
-	logger.info(rootPath);
-	// Ts-morph project management
-	const activeDirectories = readActiveDirectoriesConfiguration(
-		rootPath,
-	);
-	logger.info("[CodeStory] Active directories read");
-	logger.info(activeDirectories);
-	const projectManagement = await getProject(activeDirectories);
-	logger.info("[CodeStory] Project management created");
+  // Project root here
+  postHogClient.capture({
+    distinctId: env.machineId,
+    event: "extension_activated",
+  });
+  let rootPath = workspace.rootPath;
+  if (!rootPath) {
+    rootPath = "";
+  }
+  if (rootPath === "") {
+    window.showErrorMessage("Please open a folder in VS Code to use CodeStory");
+    return;
+  }
+  const repoName = await getGitRepoName(rootPath);
+  const repoHash = await getGitCurrentHash(rootPath);
+  // Get the storage object here
+  const codeStoryStorage = await loadOrSaveToStorage(context.globalStorageUri.fsPath, rootPath);
+  logger.info(codeStoryStorage);
+  logger.info(rootPath);
+  // Ts-morph project management
+  const activeDirectories = readActiveDirectoriesConfiguration(rootPath);
+  logger.info("[CodeStory] Active directories read");
+  logger.info(activeDirectories);
+  const projectManagement = await getProject(activeDirectories);
+  logger.info("[CodeStory] Project management created");
 
-	// Create an instance of the progressive indexer
-	const indexer = new ProgressiveIndexer();
-	// Re-index or keep going as required (we will boost this tomorrow)
-	// const symbolWithEmbeddings = await indexRepository(
-	//   codeStoryStorage,
-	//   projectManagement,
-	//   context.globalStorageUri.fsPath,
-	//   rootPath
-	// );
-	logger.info("[CodeStory] Indexing complete");
-	const embeddingsIndex = new EmbeddingsSearch([]);
-	indexer.on("partialData", (partialData) => {
-		logger.info("[CodeStory] Partial data received");
-		// Use partialData to update embeddingsIndex
-		embeddingsIndex.updateNodes(partialData);
-	});
-	indexer.indexRepository(
-		codeStoryStorage,
-		projectManagement,
-		context.globalStorageUri.fsPath,
-		rootPath,
-	);
+  // Create an instance of the progressive indexer
+  const indexer = new ProgressiveIndexer();
+  // Re-index or keep going as required (we will boost this tomorrow)
+  // const symbolWithEmbeddings = await indexRepository(
+  //   codeStoryStorage,
+  //   projectManagement,
+  //   context.globalStorageUri.fsPath,
+  //   rootPath
+  // );
+  logger.info("[CodeStory] Indexing complete");
+  const embeddingsIndex = new EmbeddingsSearch([]);
+  indexer.on("partialData", (partialData) => {
+    logger.info("[CodeStory] Partial data received");
+    // Use partialData to update embeddingsIndex
+    embeddingsIndex.updateNodes(partialData);
+  });
+  indexer.indexRepository(
+    codeStoryStorage,
+    projectManagement,
+    context.globalStorageUri.fsPath,
+    rootPath
+  );
 
-	// Get the code graph
-	const codeGraph = generateCodeGraph(projectManagement);
+  // Get the code graph
+  const codeGraph = generateCodeGraph(projectManagement);
 
-	// Register the agent view provider
-	const agentViewProvider = new AgentViewProvider(context.extensionUri);
-	context.subscriptions.push(
-		window.registerWebviewViewProvider(AgentViewProvider.viewType, agentViewProvider, {
-			webviewOptions: { retainContextWhenHidden: true },
-		})
-	);
+  // Register the agent view provider
+  const agentViewProvider = new AgentViewProvider(context.extensionUri);
+  context.subscriptions.push(
+    window.registerWebviewViewProvider(AgentViewProvider.viewType, agentViewProvider, {
+      webviewOptions: { retainContextWhenHidden: true },
+    })
+  );
 
-	context.subscriptions.push(
-		debug(
-			// TODO(codestory): Fix this properly later on
-			agentViewProvider,
-			embeddingsIndex,
-			projectManagement,
-			codeGraph,
-			repoName,
-			repoHash,
-			rootPath ?? "",
-		)
-	);
+  context.subscriptions.push(
+    debug(
+      // TODO(codestory): Fix this properly later on
+      agentViewProvider,
+      embeddingsIndex,
+      projectManagement,
+      codeGraph,
+      repoName,
+      repoHash,
+      rootPath ?? ""
+    )
+  );
 
-	// Create the copy settings from vscode command for the extension
-	const registerCopySettingsCommand = commands.registerCommand("webview.copySettings", async () => {
-		await copySettings(rootPath ?? "", logger);
-	});
+  // Create the copy settings from vscode command for the extension
+  const registerCopySettingsCommand = commands.registerCommand(
+    "webview.copySettings",
+    async () => {
+      await copySettings(rootPath ?? "", logger);
+    }
+  );
 
-	// Register the codestory view provider
-	// Create a new ChatGPTViewProvider instance and register it with the extension's context
-	const provider = new CodeStoryViewProvider(context.extensionUri, new Date());
-	context.subscriptions.push(
-		window.registerWebviewViewProvider(CodeStoryViewProvider.viewType, provider, {
-			webviewOptions: { retainContextWhenHidden: true },
-		})
-	);
+  // Register the codestory view provider
+  // Create a new ChatGPTViewProvider instance and register it with the extension's context
+  const provider = new CodeStoryViewProvider(context.extensionUri, new Date());
+  context.subscriptions.push(
+    window.registerWebviewViewProvider(CodeStoryViewProvider.viewType, provider, {
+      webviewOptions: { retainContextWhenHidden: true },
+    })
+  );
 
-	// Now we want to register the HC
-	context.subscriptions.push(healthCheck(context, provider, repoName, repoHash));
-	commands.executeCommand("codestory.healthCheck");
+  // Now we want to register the HC
+  context.subscriptions.push(healthCheck(context, provider, repoName, repoHash));
+  commands.executeCommand("codestory.healthCheck");
 
+  // We register the search command
+  // Semantic search
+  context.subscriptions.push(
+    search(provider, embeddingsIndex, repoName, repoHash),
+    openFile(logger)
+  );
 
-	// We register the search command
-	// Semantic search
-	context.subscriptions.push(
-		search(
-			provider,
-			embeddingsIndex,
-			repoName,
-			repoHash,
-		),
-		openFile(
-			logger,
-		)
-	);
+  let trackCodeSymbolChanges = new TrackCodeSymbolChanges(
+    projectManagement,
+    rootPath ?? "",
+    logger
+  );
+  const timeKeeperFileSaved = new TimeKeeper(FILE_SAVE_TIME_PERIOD);
+  const codeBlockDescriptionGenerator = new CodeBlockChangeDescriptionGenerator(logger);
+  const filesChangedFromLastCommit = await fileStateFromPreviousCommit(rootPath ?? "", logger);
 
+  for (const fileChanged of filesChangedFromLastCommit) {
+    await trackCodeSymbolChanges.filesChangedSinceLastCommit(
+      fileChanged.filePath,
+      fileChanged.fileContent
+    );
+  }
 
-	let trackCodeSymbolChanges = new TrackCodeSymbolChanges(projectManagement, rootPath ?? "", logger);
-	const timeKeeperFileSaved = new TimeKeeper(FILE_SAVE_TIME_PERIOD);
-	const codeBlockDescriptionGenerator = new CodeBlockChangeDescriptionGenerator(
-		logger,
-	);
-	const filesChangedFromLastCommit = await fileStateFromPreviousCommit(
-		rootPath ?? "",
-		logger,
-	);
+  // Also track the documents when they were last opened
+  context.subscriptions.push(
+    workspace.onDidOpenTextDocument(async (doc) => {
+      const uri = doc.uri;
+      await trackCodeSymbolChanges.fileOpened(uri, logger);
+    })
+  );
 
-	for (const fileChanged of filesChangedFromLastCommit) {
-		await trackCodeSymbolChanges.filesChangedSinceLastCommit(
-			fileChanged.filePath,
-			fileChanged.fileContent,
-		);
-	};
+  // Now we parse the documents on save as well
+  context.subscriptions.push(
+    workspace.onDidSaveTextDocument(async (doc) => {
+      const uri = doc.uri;
+      const fsPath = doc.uri.fsPath;
+      await trackCodeSymbolChanges.fileSaved(uri, logger);
+      await triggerCodeSymbolChange(
+        provider,
+        trackCodeSymbolChanges,
+        timeKeeperFileSaved,
+        fsPath,
+        codeBlockDescriptionGenerator,
+        logger
+      );
+    })
+  );
 
-	// Also track the documents when they were last opened
-	context.subscriptions.push(
-		workspace.onDidOpenTextDocument(async (doc) => {
-			const uri = doc.uri;
-			await trackCodeSymbolChanges.fileOpened(uri, logger);
-		})
-	);
-
-	// Now we parse the documents on save as well
-	context.subscriptions.push(
-		workspace.onDidSaveTextDocument(async (doc) => {
-			const uri = doc.uri;
-			const fsPath = doc.uri.fsPath;
-			await trackCodeSymbolChanges.fileSaved(uri, logger);
-			await triggerCodeSymbolChange(
-				provider,
-				trackCodeSymbolChanges,
-				timeKeeperFileSaved,
-				fsPath,
-				codeBlockDescriptionGenerator,
-				logger,
-			);
-		})
-	);
-
-	// Add git commit to the subscriptions here
-	// Git commit
-	context.subscriptions.push(
-		gitCommit(logger, repoName, repoHash),
-	);
-	context.subscriptions.push(registerCopySettingsCommand);
+  // Add git commit to the subscriptions here
+  // Git commit
+  context.subscriptions.push(gitCommit(logger, repoName, repoHash));
+  context.subscriptions.push(registerCopySettingsCommand);
 }
---
+++ extensions/codestory/src/utilities/setupAntonBackend.ts
@@ -0,0 +1,233 @@
+// We are going to setup the anton backend here
+
+import fetch from "node-fetch";
+import { workspace, window, ProgressLocation, extensions } from 'vscode';
+import * as path from "path";
+import * as fs from "fs";
+import fkill from "fkill";
+import { promisify } from "util";
+import { spawn } from "child_process";
+import { exec } from "child_process";
+import * as os from "os";
+import { downloadFromGCPBucket } from './gcpBucket';
+
+
+export function getContinueServerUrl() {
+	// Passed in from launch.json
+	if (process.env.CONTINUE_SERVER_URL) {
+		return process.env.CONTINUE_SERVER_URL;
+	}
+
+	return (
+		workspace.getConfiguration("aide").get<string>("pythonServerUrl") ||
+		"http://localhost:424242"
+	);
+}
+
+async function runCommand(cmd: string): Promise<[string, string | undefined]> {
+	var stdout = "";
+	var stderr = "";
+	try {
+		var { stdout, stderr } = await promisify(exec)(cmd, {
+			shell: process.platform === "win32" ? "powershell.exe" : undefined,
+		});
+	} catch (e: any) {
+		stderr = e.stderr;
+		stdout = e.stdout;
+	}
+
+	const stderrOrUndefined = stderr === "" ? undefined : stderr;
+	return [stdout, stderrOrUndefined];
+}
+
+export function getExtensionVersion() {
+	const extension = extensions.getExtension("codestory-ghost.codestoryai");
+	return extension?.packageJSON.version || "";
+}
+
+async function checkServerRunning(serverUrl: string): Promise<boolean> {
+	// Check if already running by calling /health
+	try {
+		const response = await fetch(`${serverUrl}/health`);
+		if (response.status === 200) {
+			console.log("Continue python server already running");
+			return true;
+		} else {
+			return false;
+		}
+	} catch (e) {
+		return false;
+	}
+}
+
+async function checkOrKillRunningServer(serverUrl: string): Promise<boolean> {
+	const serverRunning = await checkServerRunning(serverUrl);
+	if (serverRunning) {
+		console.log("Killing server from old version of Aide");
+		try {
+			await fkill(":424242", { force: true });
+		} catch (e: any) {
+			if (!e.message.includes("Process doesn't exist")) {
+				console.log("Failed to kill old server:", e);
+			}
+		}
+	}
+	return false;
+}
+
+function serverPath(extensionGlobalStorage: string): string {
+	const sPath = path.join(extensionGlobalStorage, "server");
+	if (!fs.existsSync(sPath)) {
+		fs.mkdirSync(sPath);
+	}
+	return sPath;
+}
+
+function serverVersionPath(extensionGlobalStorage: string): string {
+	return path.join(serverPath(extensionGlobalStorage), "server_version.txt");
+}
+
+
+export async function startAidePythonBackend(extensionBasePath: string) {
+	// Check vscode settings
+	const serverUrl = getContinueServerUrl();
+	if (serverUrl !== "http://localhost:424242") {
+		console.log("Continue server is being run manually, skipping start");
+		return;
+	}
+
+	// Check if server is already running
+	if (await checkOrKillRunningServer(serverUrl)) {
+		console.log("Continue server already running");
+		return;
+	}
+
+	// Download the server executable
+	const bucket = "aide-binary";
+	const fileName =
+		os.platform() === "win32"
+			? "windows/run.exe"
+			: os.platform() === "darwin"
+				? "mac/run"
+				: "linux/run";
+
+	const destination = path.join(
+		extensionBasePath,
+		"server",
+		"exe",
+		`run${os.platform() === "win32" ? ".exe" : ""}`
+	);
+
+	// First, check if the server is already downloaded
+	let shouldDownload = true;
+	if (fs.existsSync(destination)) {
+		// Check if the server is the correct version
+		const serverVersion = fs.readFileSync(
+			serverVersionPath(extensionBasePath), "utf8",
+		);
+		if (serverVersion === getExtensionVersion()) {
+			// The current version is already up and running, no need to run
+			console.log("Continue server already downloaded");
+			shouldDownload = false;
+		} else {
+			fs.unlinkSync(destination);
+		}
+	}
+
+	if (shouldDownload) {
+		await window.withProgress(
+			{
+				location: ProgressLocation.Notification,
+				title: "Installing Continue server...",
+				cancellable: false,
+			},
+			async () => {
+				await downloadFromGCPBucket(bucket, fileName, destination);
+			}
+		);
+	}
+
+	console.log("Downloaded server executable at ", destination);
+	// Get name of the corresponding executable for platform
+	if (os.platform() === "darwin") {
+		// Add necessary permissions
+		fs.chmodSync(destination, 0o7_5_5);
+		await runCommand(`xattr -dr com.apple.quarantine ${destination}`);
+	} else if (os.platform() === "linux") {
+		// Add necessary permissions
+		fs.chmodSync(destination, 0o7_5_5);
+	}
+
+	// Validate that the file exists
+	if (!fs.existsSync(destination)) {
+		const errText = `- Failed to install Aide server.`;
+		window.showErrorMessage(errText);
+		throw new Error(errText);
+	}
+
+	// Run the executable
+	console.log("Starting Aide server");
+	let attempts = 0;
+	let maxAttempts = 5;
+	let delay = 1000; // Delay between each attempt in milliseconds
+
+	const spawnChild = () => {
+		const retry = () => {
+			attempts++;
+			console.log(
+				`Error caught (likely EBUSY). Retrying attempt ${attempts}...`
+			);
+			setTimeout(spawnChild, delay);
+		};
+		try {
+			// NodeJS bug requires not using detached on Windows, otherwise windowsHide is ineffective
+			// Otherwise, detach is preferable
+			const windowsSettings = {
+				windowsHide: true,
+			};
+			const macLinuxSettings = {
+				detached: true,
+				stdio: "ignore",
+			};
+			const settings: any =
+				os.platform() === "win32" ? windowsSettings : macLinuxSettings;
+
+			// Spawn the server
+			const child = spawn(destination, settings);
+
+			// Either unref to avoid zombie process, or listen to events because you can
+			if (os.platform() === "win32") {
+				child.stdout.on("data", (data: any) => {
+					// console.log(`stdout: ${data}`);
+				});
+				child.stderr.on("data", (data: any) => {
+					console.log(`stderr: ${data}`);
+				});
+				child.on("error", (err: any) => {
+					if (attempts < maxAttempts) {
+						retry();
+					} else {
+						console.error("Failed to start subprocess.", err);
+					}
+				});
+				child.on("exit", (code: any, signal: any) => {
+					console.log("Subprocess exited with code", code, signal);
+				});
+				child.on("close", (code: any, signal: any) => {
+					console.log("Subprocess closed with code", code, signal);
+				});
+			} else {
+				child.unref();
+			}
+		} catch (e: any) {
+			console.log("Error starting server:", e);
+			retry();
+		}
+	};
+
+	spawnChild();
+
+	// Write the current version of vscode extension to a file called server_version.txt
+	fs.writeFileSync(serverVersionPath(extensionBasePath), getExtensionVersion());
+}
+
