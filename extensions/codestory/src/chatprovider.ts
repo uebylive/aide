59a5d1eeefec46656acf1e64940f2679736f700e
skcd <sandeepkumarpani888@gmail.com>
2023-08-17 21:28:22 -0700

    [chat] add promise

--- extensions/codestory/src/chatprovider.ts
+++ extensions/codestory/src/chatprovider.ts
@@ -272,7 +272,7 @@ export class CSChatProvider implements vscode.InteractiveSessionProvider {
 		return new CSChatRequest(session, context.toString());
 	}
 
-	async provideResponseWithProgress(request: CSChatRequest, progress: vscode.Progress<CSChatProgress>, token: CSChatCancellationToken): vscode.ProviderResult<CSChatResponseForProgress> {
+	async provideResponseWithProgress(request: CSChatRequest, progress: vscode.Progress<CSChatProgress>, token: CSChatCancellationToken): Promise<vscode.ProviderResult<CSChatResponseForProgress>> {
 		logger.info('provideResponseWithProgress', request, progress, token);
 		if (request.message.toString().startsWith('/help')) {
 			progress.report(new CSChatProgressContent(
