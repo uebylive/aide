2193adb364ddae26755463f2b2812cbccd97a2fc
Naresh <ghostwriternr@gmail.com>
2023-09-04 21:40:06 -0700

    Register shift+shift to focus on omnibar

--- src/vs/workbench/contrib/chat/browser/actions/chatHoverActions.ts
+++ src/vs/workbench/contrib/chat/browser/actions/chatHoverActions.ts
@@ -2,7 +2,7 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import { KeyCode, KeyMod } from 'vs/base/common/keyCodes';
+import { KeyChord, KeyMod } from 'vs/base/common/keyCodes';
 import { ServicesAccessor } from 'vs/editor/browser/editorExtensions';
 import { localize } from 'vs/nls';
 import { Action2 } from 'vs/platform/actions/common/actions';
@@ -18,24 +18,41 @@ import { IHoverChatService } from 'vs/workbench/contrib/chat/browser/chat';
  * @param label The label of the provider
  * @returns An action that will toggle the hover chat for this provider
  */
-export function getHoverActionForProvider(id: string, label: string) {
-	return class ToggleHoverChatAction extends Action2 {
-		constructor() {
-			super({
-				id: `workbench.action.toggleHoverChat.${id}`,
-				category: CHAT_CATEGORY,
-				title: { value: localize('interactiveHoverSession.toggle', "Toggle Hover Chat ({0})", label), original: `Toggle Hover Chat (${label})` },
-				keybinding: {
-					weight: KeybindingWeight.WorkbenchContrib,
-					primary: KeyMod.CtrlCmd | KeyMod.Shift | KeyCode.KeyA,
-				},
-				f1: true
-			});
-		}
+export function getHoverActionsForProvider(id: string, label: string) {
+	return [
+		class ToggleHoverChatAction extends Action2 {
+			constructor() {
+				super({
+					id: `workbench.action.toggleHoverChat.${id}`,
+					category: CHAT_CATEGORY,
+					title: { value: localize('interactiveHoverSession.toggle', "Toggle Hover Chat ({0})", label), original: `Toggle Hover Chat (${label})` },
+					f1: true
+				});
+			}
 
-		override run(accessor: ServicesAccessor, query?: string): void {
-			const hoverChatService = accessor.get(IHoverChatService);
-			hoverChatService.toggle();
+			override run(accessor: ServicesAccessor, query?: string): void {
+				const hoverChatService = accessor.get(IHoverChatService);
+				hoverChatService.toggle();
+			}
+		},
+		class FocusHoverChatAction extends Action2 {
+			constructor() {
+				super({
+					id: `workbench.action.focusHoverChat.${id}`,
+					category: CHAT_CATEGORY,
+					title: { value: localize('interactiveHoverSession.focus', "Focus Hover Chat ({0})", label), original: `Focus Hover Chat (${label})` },
+					keybinding: {
+						weight: KeybindingWeight.EditorContrib,
+						primary: KeyChord(KeyMod.Shift, KeyMod.Shift),
+					},
+					f1: true
+				});
+			}
+
+			override run(accessor: ServicesAccessor, query?: string): void {
+				const hoverChatService = accessor.get(IHoverChatService);
+				hoverChatService.open();
+			}
 		}
-	};
+	];
 }
