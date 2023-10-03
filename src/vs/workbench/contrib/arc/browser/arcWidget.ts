9da311a307b9f6a4aa4de02744f81a631bf014fa
Naresh <ghostwriternr@gmail.com>
2023-10-02 19:01:55 -0700

    Render chat within arc

--- src/vs/workbench/contrib/arc/browser/arcWidget.ts
+++ src/vs/workbench/contrib/arc/browser/arcWidget.ts
@@ -11,12 +11,16 @@ import { IArcWidget, IArcWidgetService } from 'vs/workbench/contrib/arc/browser/
 import { IArcService } from 'vs/workbench/contrib/arc/common/arcService';
 import { IArcViewModel } from 'vs/workbench/contrib/arc/common/arcViewModel';
 import { IWorkbenchLayoutService } from 'vs/workbench/services/layout/browser/layoutService';
+import { quickInputForeground, quickInputBackground, inputBackground } from 'vs/platform/theme/common/colorRegistry';
+import { ChatWidget } from 'vs/workbench/contrib/chat/browser/chatWidget';
+import { IChatService } from 'vs/workbench/contrib/chat/common/chatService';
 
 export class ArcWidgetService extends Disposable implements IArcWidgetService {
 	declare readonly _serviceBrand: undefined;
 
 	private _widget: ArcWidget | undefined;
 	private _container: HTMLElement | undefined;
+	private _chatContainer: HTMLElement | undefined;
 
 	constructor(
 		@IWorkbenchLayoutService private readonly workbenchLayoutService: IWorkbenchLayoutService,
@@ -29,8 +33,12 @@ export class ArcWidgetService extends Disposable implements IArcWidgetService {
 		const arcContainer = document.createElement('div');
 		arcContainer.classList.add('arc-widget-container');
 		this._container = arcContainer;
+		const chatContainer = document.createElement('div');
+		chatContainer.classList.add('arc-widget-chat-container');
+		this._container.appendChild(chatContainer);
+		this._chatContainer = chatContainer;
 		this._widget = this.instantiationService.createInstance(ArcWidget, 'cs-arc');
-		this._widget.render(this._container);
+		this._widget.render(this._chatContainer);
 		this.workbenchLayoutService.container.appendChild(this._container);
 	}
 
@@ -51,7 +59,12 @@ export class ArcWidgetService extends Disposable implements IArcWidgetService {
 }
 
 export class ArcWidget extends Disposable implements IArcWidget {
+	declare readonly _serviceBrand: undefined;
+
 	public static readonly CONTRIBS: { new(...args: [IArcWidget, ...any]): any }[] = [];
+
+	private chatWidget: ChatWidget | undefined;
+	private _currentQuery: string | undefined;
 
 	private _viewModel: IArcViewModel | undefined;
 	private set viewModel(viewModel: IArcViewModel | undefined) {
@@ -69,16 +82,44 @@ export class ArcWidget extends Disposable implements IArcWidget {
 	constructor(
 		private readonly providerId: string,
 		@IArcService private readonly arcService: IArcService,
+		@IInstantiationService private readonly instantiationService: IInstantiationService,
+		@IChatService private readonly chatService: IChatService,
 	) {
 		super();
-		this.updateModel();
 	}
 
 	render(parent: HTMLElement): void {
-		parent.innerText = 'Arc Widget';
+		if (this.chatWidget) {
+			throw new Error('Cannot render chat twice');
+		}
+
+		this.chatWidget = this.instantiationService.createInstance(
+			ChatWidget,
+			{ resource: true },
+			{
+				listForeground: quickInputForeground,
+				listBackground: quickInputBackground,
+				inputEditorBackground: inputBackground,
+				resultEditorBackground: quickInputBackground
+			}
+		);
+		this.chatWidget.render(parent);
+		this.chatWidget.setVisible(true);
+		this.chatWidget.layout(1000, 1000);
+		this.updateModel();
+
+		this._register(this.chatWidget.inputEditor.onDidChangeModelContent((e) => {
+			this._currentQuery = this.chatWidget?.inputEditor.getValue();
+		}));
 	}
 
 	private updateModel(): void {
 		this.arcService.startSession(this.providerId, CancellationToken.None);
+		const chatSession = this.chatService.startSession('cs-chat', CancellationToken.None);
+		if (!chatSession) {
+			throw new Error('Could not start chat session');
+		}
+
+		this.chatWidget?.setModel(chatSession, { inputValue: this._currentQuery });
 	}
 }
