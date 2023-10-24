92522d1d573c3f1c3554fc58bf50026ac96b4fc1
Naresh <ghostwriternr@gmail.com>
2023-10-24 20:38:49 +0530

    Render a single, working editor block

--- src/vs/workbench/contrib/csAgent/browser/csAgentViewPane.ts
+++ src/vs/workbench/contrib/csAgent/browser/csAgentViewPane.ts
@@ -7,15 +7,24 @@ import { IConfigurationService } from 'vs/platform/configuration/common/configur
 import { IContextKeyService } from 'vs/platform/contextkey/common/contextkey';
 import { IContextMenuService } from 'vs/platform/contextview/browser/contextView';
 import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
+import { ServiceCollection } from 'vs/platform/instantiation/common/serviceCollection';
 import { IKeybindingService } from 'vs/platform/keybinding/common/keybinding';
 import { IOpenerService } from 'vs/platform/opener/common/opener';
 import { ITelemetryService } from 'vs/platform/telemetry/common/telemetry';
 import { IThemeService } from 'vs/platform/theme/common/themeService';
 import { IViewPaneOptions, ViewPane } from 'vs/workbench/browser/parts/views/viewPane';
 import { IViewDescriptorService } from 'vs/workbench/common/views';
+import { CSAgentWidget } from 'vs/workbench/contrib/csAgent/browser/csAgentWidget';
+
+// interface IViewPaneState extends IViewState {
+// 	sessionId?: string;
+// }
 
 export class CSAgentViewPane extends ViewPane {
 	static readonly ID: string = 'workbench.panel.csAgentAuxiliaryBar';
+
+	private _widget!: CSAgentWidget;
+	get widget(): CSAgentWidget { return this._widget; }
 
 	constructor(
 		options: IViewPaneOptions,
@@ -30,5 +39,25 @@ export class CSAgentViewPane extends ViewPane {
 		@ITelemetryService telemetryService: ITelemetryService
 	) {
 		super(options, keybindingService, contextMenuService, configurationService, contextKeyService, viewDescriptorService, instantiationService, openerService, themeService, telemetryService);
+	}
+
+	protected override renderBody(parent: HTMLElement): void {
+		try {
+			super.renderBody(parent);
+
+			const scopedInstantiationService = this.instantiationService.createChild(new ServiceCollection([IContextKeyService, this.scopedContextKeyService]));
+
+			this._widget = this._register(scopedInstantiationService.createInstance(
+				CSAgentWidget,
+			));
+			this._widget.render(parent);
+		} catch (e) {
+			throw e;
+		}
+	}
+
+	protected override layoutBody(height: number, width: number): void {
+		super.layoutBody(height, width);
+		this._widget.layout(height, width);
 	}
 }
--- src/vs/workbench/contrib/csAgent/common/csAgentViewModel.ts
+++ src/vs/workbench/contrib/csAgent/common/csAgentViewModel.ts
@@ -3,12 +3,6 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { ICSAgentViewModel } from 'vs/workbench/contrib/csAgent/common/csAgentViewModel';
-
-export const VIEWLET_ID = 'workbench.viewlet.csAgent';
-export const PANEL_ID = 'workbench.panel.csAgentAuxiliaryBar';
-export const VIEW_ID = 'workbench.view.csAgent';
-
-export interface ICSAgentWidget {
-	readonly viewModel: ICSAgentViewModel | undefined;
+export interface ICSAgentViewModel {
+	readonly sessionId: string;
 }
