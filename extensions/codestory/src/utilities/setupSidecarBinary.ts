9af8e8411c6b01b013e903234af26eb80246168a
skcd <sandeepkumarpani888@gmail.com>
2024-02-07 20:55:07 +0000

    [ide] fix update hash

--- extensions/codestory/src/utilities/setupSidecarBinary.ts
+++ extensions/codestory/src/utilities/setupSidecarBinary.ts
@@ -45,7 +45,7 @@ export function getSidecarBinaryURL() {
 // if the version we are looking at is okay, or we need to download a new binary
 // for now, lets keep it as it is and figure out a way to update the hash on
 // important updates
-export const SIDECAR_VERSION = 'b88efe7c5dad149b42623d08bda453ccf8f65f0b2f6fedf21b9c524fecf2624c';
+export const SIDECAR_VERSION = '9eabbf417673a15091631a215efa170f8eb33dff2fe17d747981efd74f738193';
 
 async function checkCorrectVersionRunning(url: string): Promise<boolean> {
 	try {
