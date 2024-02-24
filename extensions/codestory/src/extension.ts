6f01de6feefd381d6607c9be706b4b2a01de367b
skcd <sandeepkumarpani888@gmail.com>
2024-02-24 02:59:21 +0000

    [ide] log the exec path for vscode

--- extensions/codestory/src/extension.ts
+++ extensions/codestory/src/extension.ts
@@ -125,6 +125,8 @@ export async function activate(context: ExtensionContext) {
 
 	// Get model selection configuration
 	const modelConfiguration = await modelSelection.getConfiguration();
+	const execPath = process.execPath;
+	console.log('Exec path:' + execPath);
 	console.log('Model configuration:' + JSON.stringify(modelConfiguration));
 	// Setup the sidecar client here
 	const sidecarUrl = await startSidecarBinary(context.globalStorageUri.fsPath, env.appRoot);
