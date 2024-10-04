491af9d0bb51fb37c4d4672f92759c68f1ad2955
sartoshi-foot-dao <99770068+sartoshi-foot-dao@users.noreply.github.com>
2024-10-04 16:43:35 +0100

    [ide] fix name

--- extensions/codestory/src/completions/providers/editorSessionProvider.ts
+++ extensions/codestory/src/completions/providers/editorSessionProvider.ts
@@ -245,7 +245,7 @@ export async function provideInteractiveEditorResponse(
 			is_plan_generation: false,
 			is_plan_execution_until: null,
 			is_plan_append: false,
-			is_lsp_run: false,
+			with_lsp_enrichment: false,
 			is_plan_drop_from: null,
 		}
 	};
